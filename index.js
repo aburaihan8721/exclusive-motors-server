f7896fb5f1670a10292d805834d887a5b0275da6
Abu Raihan <aburaihan8721@gmail.comm>
2021-11-13 19:11:22 +0600

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
-
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 const app = express();
 
@@ -17,18 +17,75 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbl8m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 async function run() {
   try {
     await client.connect();
-    console.log("database connected successfully");
+    const database = client.db("exclusiveMotors");
+    console.log("database connected");
+    // collection
+    const carsCollection = database.collection("carsCollection");
+    const reviewCollection = database.collection("reviewCollection");
+    const addressCollection = database.collection("addressCollection");
 
-    // create a document to insert
+    // ==================POST METHODS===========================
+    // add cars data
+    app.post("/cars", async (req, res) => {
+      const car = req.body;
+      const result = await carsCollection.insertOne(car);
+      console.log(result);
+      res.json(result);
+    });
+
+    // add review data
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      console.log(result);
+      res.json(result);
+    });
+
+    // add address data
+    app.post("/address", async (req, res) => {
+      const singleAddress = req.body;
+      const result = await addressCollection.insertOne(singleAddress);
+      console.log(result);
+      res.json(result);
+    });
+
+
+    
+    // ==================GET METHODS===========================
+    // get all cars data
+    app.get("/cars", async (req, res) => {
+      const cursor = carsCollection.find({});
+      const cars = await cursor.toArray();
+      res.send(cars);
+    });
+
+    // get single cars data
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const car = await carsCollection.findOne(query);
+      res.send(car);
+    });
+
+    // get all reviews data
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    // get all address
+    app.get("/address", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const address = await cursor.toArray();
+      res.send(address);
+    });
+
+
+
   } finally {
     // await client.close();
   }
@@ -42,5 +99,7 @@ app.get("/", (req, res) => {
 });
 // Basic url
 app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
+
+// ==============end===============
